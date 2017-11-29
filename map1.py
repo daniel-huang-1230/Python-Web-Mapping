65b8c38425160df16b7c4ec611792ec653ad68ad
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-11-29 03:10:13 -0800

    implementation done, readme todo

--- map1.py
+++ map1.py
@@ -2,7 +2,7 @@ import folium, pandas
 # first create a map object
 map = folium.Map(location=[38.58, -99], zoom_start=6, tiles="Mapbox Bright")
 data = pandas.read_csv("Volcanoes_USA.txt")
-fg = folium.FeatureGroup(name='My Map')
+fgv = folium.FeatureGroup(name='Volcanoes')
 # convert the pandas series to python list
 lat = list(data['LAT'])
 lon = list(data['LON'])
@@ -19,16 +19,19 @@ def color_producer(elevation):
 
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, fill=True, color='gray', fill_opacity=0.7,
+    fgv.add_child(folium.CircleMarker(location=[lt, ln], radius=6, fill=True, color='gray', fill_opacity=0.7,
                                      fill_color=color_producer(el), popup=str(el)+" m"))
 
+fgp = folium.FeatureGroup(name='Population')
 # add another layer: the GeoJson object
 # the x inside the lambda expression is 'feature' in geojson object ----can refer to the doc of folium library
-fg.add_child(folium.GeoJson(data=(open('world.json','r', encoding='utf-8-sig')).read(),
+fgp.add_child(folium.GeoJson(data=(open('world.json','r', encoding='utf-8-sig')).read(),
                             style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
                             else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
-map.add_child(fg)
-
+map.add_child(fgp)
+map.add_child(fgv)
+# it's important to add the layer control object after we've added all feature group(s)
+map.add_child(folium.LayerControl())
 map.save('Map1.html')
 
 
