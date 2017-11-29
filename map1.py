b00b3d784cc5d1416e7ae6d5d490aa79a087ba6d
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-11-29 01:03:35 -0800

    circle markers added

--- map1.py
+++ map1.py
@@ -1,20 +1,28 @@
 import folium, pandas
-#first create a map object
-map=folium.Map(location=[38.58, -99], zoom_start=6, tiles="Mapbox Bright")
+# first create a map object
+map = folium.Map(location=[38.58, -99], zoom_start=6, tiles="Mapbox Bright")
 data = pandas.read_csv("Volcanoes_USA.txt")
 fg = folium.FeatureGroup(name='My Map')
-
-#convert the pandas series to python list
+# convert the pandas series to python list
 lat = list(data['LAT'])
 lon = list(data['LON'])
 elev = list(data['ELEV'])
 
+def color_producer(elevation):
+    if elevation < 1500:
+        return 'green'
+    elif elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, fill=True, color='gray', fill_opacity=0.7, fill_color=color_producer(el), popup=str(el)+" m"))
 
 map.add_child(fg)
 
 map.save('Map1.html')
 
+
+# a function that determines the color of the marker based on elevation
